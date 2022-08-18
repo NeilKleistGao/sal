4fded3a7e945be8eae379749343c526cfece0348
NeilKleistGao <neilkleist@gmail.com>
2022-08-18 18:11:43 +0800

    Add tests

--- src/main/scala/sal/STVisitor.scala
+++ src/main/scala/sal/STVisitor.scala
@@ -3,17 +3,23 @@ package sal
 import org.antlr.v4.runtime.{CharStreams}
 import scala.util.Try
 import scala.collection.JavaConverters._
+import java.lang.StringBuilder
 
 class STVisitor extends sal.parser.SalParserBaseVisitor[STNode] {
   import sal.parser.SalParser
   import STVisitor._
 
   private val typeCtx = types.Context()
+  private val errors = new StringBuilder("\n")
 
   override def visitLit(ctx: SalParser.LitContext) = LitNode(ctx.getText)
 
-  override def visitProgram(ctx: SalParser.ProgramContext) =
-    ProgramNode(ctx.statement.asScala.toList.map((s) => visitStatement(s)))
+  override def visitProgram(ctx: SalParser.ProgramContext) = {
+    val list = ctx.statement.asScala.toList.map((s) => visitStatement(s))
+    if (errors.length() <= 1)
+      ProgramNode(list)
+    else throw SalException(errors.toString())
+  }
 
   override def visitStatement(ctx: SalParser.StatementContext) = StatementNode(visitValue(ctx.value))
 
@@ -24,9 +30,16 @@ class STVisitor extends sal.parser.SalParserBaseVisitor[STNode] {
     val name = ctx.ID().getText
     val tp = visitTypeName(ctx.typeName)
     val exp = visitExpression(ctx.expression)
-    typeCtx += (name, tp.salType)
+
+    try
+      typeCtx += (name, tp.salType)
+    catch {
+      case SalException(info) => errors.append(s"  $info\n")
+      case _: Throwable => errors.append("  unknown error.\n")
+    }
+
     if (!typeCtx.require(name, exp.salType))
-      throw new SalException(s"$name got ${exp.salType}, but ${tp.salType} is requireed.")
+      errors.append(s"  $name got ${exp.salType}, but ${tp.salType} is required.\n")
 
     ValueNode(name, tp, exp)
   }
