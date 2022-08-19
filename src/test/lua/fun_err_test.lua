99157785e2c5bb85b1ede2a7af53b7a07ded25ae
NeilKleistGao <neilkleist@gmail.com>
2022-08-20 00:47:19 +0800

    Add test

---
+++ src/test/lua/fun_err_test.lua
@@ -0,0 +1,4 @@
+--[[
+  return value of f got string, but int is required.
+  y got int, but string is required.
+]]
--- src/test/scala/sal/FunDecTest.scala
+++ src/test/scala/sal/FunDecTest.scala
@@ -6,9 +6,15 @@ class FunDecTest extends DiffTest {
   test("Function Declaration Test") {
     generate(input, output)
   }
+
+  test("Function Declaration Error Test") {
+    generate(err_input, err_output)
+  }
 }
 
 object FunDecTest {
   val input = "fun_dec_test.sal"
   val output = "fun_dec_test.lua"
+  val err_input = "fun_err_test.sal"
+  val err_output = "fun_err_test.lua"
 }
