af44472736eb06b09f08b2bf1e38b16898c1b93e
NeilKleistGao <neilkleist@gmail.com>
2022-08-20 11:18:51 +0800

    Add test

--- src/test/lua/fun_err_test.lua
+++ src/test/lua/fun_err_test.lua
@@ -2,4 +2,7 @@
   return value of f got string, but int is required.
   y got int, but string is required.
   return value of h got int, but void is required.
+  xxx got void -> void, but void -> int is required.
+  return value of high1 got int -> int, but int -> void is required.
+  return value of high2 got void -> void, but int -> int is required.
 ]]
