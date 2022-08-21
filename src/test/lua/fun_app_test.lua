1cfa1b37508f06b88cfbba6f8a7f75e6e14f78dc
NeilKleistGao <neilkleist@gmail.com>
2022-08-21 11:27:12 +0800

    Add test

--- src/test/lua/fun_app_test.lua
+++ src/test/lua/fun_app_test.lua
@@ -7,3 +7,22 @@ function g(x, y)
 end
 local hg = function(p) return g(42, p) end
 local s = hg("rua")
+function h()
+  return false
+end
+local fs = h()
+function m()
+  local res = nil
+  function f(x)
+    return x
+  end
+  res = f
+  return res
+end
+local innerF = function(p) return m(p) end
+local res = innerF(42)
+function foo(x, y, z)
+  local res = nil
+  return res
+end
+local bar = function(p, p_) return foo(42, p, p_) end
