78a5a751439d2b0dc325385e3e368ca26cd2d8a8
NeilKleistGao <neilkleist@gmail.com>
2022-08-22 22:02:44 +0800

    Add tests

--- src/test/lua/val_inf_test.lua
+++ src/test/lua/val_inf_test.lua
@@ -4,3 +4,23 @@ local z1 = false --[[type: bool]]
 local z2 = true --[[type: bool]]
 local w = "\"abc\"" --[[type: string]]
 local n1 = nil --[[type: anything]]
+function f(x)
+  local res = nil
+  local y = x --[[type: int]]
+  return res
+end
+function g()
+  return 3.14
+end
+local pi = g() --[[type: float]]
+function h(x, y)
+  local res = nil
+  function hh(xx)
+    return y
+  end
+  res = hh
+  return res
+end
+local hh = h --[[type: string -> bool -> string -> bool]]
+local hhh = function(p, p_) return h("rua", p, p_) end --[[type: bool -> string -> bool]]
+local hhhh = function(p) return hh("rua", false, p) end --[[type: string -> bool]]
