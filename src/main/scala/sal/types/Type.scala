abc1aa01a7af9b0b69beeb6acae5aa057a981e48
NeilKleistGao <neilkleist@gmail.com>
2022-08-23 10:04:18 +0800

    Add type variable

--- src/main/scala/sal/types/Type.scala
+++ src/main/scala/sal/types/Type.scala
@@ -16,7 +16,6 @@ case class BuiltInType(name: String) extends Type {
   override def toString(): String = name
 }
 
-
 case class FunctionType(param: Type, res: Type) extends Type {
   override def ===(other: Type): Boolean = other match {
     case FunctionType(p, r) => p === param && r === res
@@ -27,5 +26,13 @@ case class FunctionType(param: Type, res: Type) extends Type {
   override def toString(): String = param match {
     case f: FunctionType => s"($param) -> $res"
     case _ => s"$param -> $res"
+  }
+}
+
+case class TypeVariable(val name: String) extends Type {
+  override def ===(other: Type): Boolean = other match {
+    case BuiltInType(nm) if (nm.equals("anything")) => true
+    case TypeVariable(nm) if (name.equals(nm)) => true
+    case _ => false
   }
 }
