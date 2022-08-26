166f34fccca626daabdaa7e781b9b89c4cdc7ec9
NeilKleistGao <neilkleist@gmail.com>
2022-08-26 21:11:09 +0800

    Add test

---
+++ src/test/lua/cal_err_test.lua
@@ -0,0 +1,13 @@
+--[[
+  line 1: operator LogicAnd is bool -> bool -> bool, but parameters are bool and int
+  line 2: operator LogicOr is bool -> bool -> bool, but parameters are bool and string
+  line 3: operator LogicNot is bool -> bool, but the parameter is float
+  line 4: operator LeftShift is int -> int -> int, but parameters are float and int
+  line 5: operator RightShift is int -> int -> int, but parameters are float and int
+  line 6: operator BitwiseAnd is int -> int -> int, but parameters are float and float
+  line 7: operator BitwiseOr is int -> int -> int, but parameters are float and float
+  line 8: operator BitwiseXor is int -> int -> int, but parameters are float and float
+  line 9: operator BitwiseNot is int -> int, but the parameter is float
+  line 11: xx got bool, but string is required.
+  line 12: yy got int, but float is required.
+]]
