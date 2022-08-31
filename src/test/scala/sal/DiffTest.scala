77b62a1868de6d49727990d0288085c9f5eab7e6
NeilKleistGao <neilkleist@gmail.com>
2022-08-31 10:42:07 +0800

    Refactor tests

--- src/test/scala/sal/DiffTest.scala
+++ src/test/scala/sal/DiffTest.scala
@@ -3,16 +3,35 @@ package sal
 import org.scalatest.funsuite.AnyFunSuite
 
 class DiffTest extends AnyFunSuite {
-  // TODO: add more test parameters
+  import DiffTest._
+
   def generate(from: String, to: String): Unit = {
-    import DiffTest._
     val compiler = Compiler(s"$salPath/$from")
     compiler.toLua(s"$luaPath/$to")
   }
+
+  testList.foreach((name) => test(name) {
+    generate(s"$name.sal", s"$name.lua")
+  })
 }
 
 object DiffTest {
   private val testPath = "src/test"
   private val salPath = s"$testPath/sal"
   private val luaPath = s"$testPath/lua"
+
+  private val testList = List(
+    "cal_dec_test",
+    "cal_err_test",
+    "fun_app_test",
+    "app_err_test",
+    "fun_dec_test",
+    "fun_err_test",
+    "fun_inf_test",
+    "rec_dec_test",
+    "rec_err_test",
+    "val_dec_test",
+    "val_err_test",
+    "val_inf_test"
+  )
 }
