0b8f2b13a32e3adace726524d2b4e6e04694d688
NeilKleistGao <neilkleist@gmail.com>
2022-08-31 12:50:16 +0800

    Add tests

--- src/test/lua/con_dec_test.lua
+++ src/test/lua/con_dec_test.lua
@@ -4,3 +4,18 @@ if (true) then
 else
   (print)("world")
 end
+local if_res_ = nil
+if (false) then
+  local x = 41 --[[type: int]]
+  (print)(x)
+end
+local x = false --[[type: bool]]
+local y = true --[[type: bool]]
+local if_res__ = nil
+if (x) then
+  (print)("x")
+elseif (y) then
+  (print)("y")
+else
+  (print)("z")
+end
