3bfb383a73976fc90d8ab74190b3405067bee4d4
NeilKleistGao <neilkleist@gmail.com>
2022-08-31 11:03:37 +0800

    Add merge func

--- src/main/scala/sal/types/Type.scala
+++ src/main/scala/sal/types/Type.scala
@@ -70,4 +70,8 @@ case class UnionType(lhs: Type, rhs: Type) extends Type {
   override def toString(): String = s"$lhs | $rhs"
 
   def :>(other: Type) = (other === lhs) || (other === rhs)
+
+  def :+(other: Type): UnionType =
+    if (:>(other)) this
+    else UnionType(this, other) 
 } 
