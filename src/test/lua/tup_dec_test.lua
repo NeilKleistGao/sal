4d9daab445df02fd8ea4caa6321949fcbda6b15c
NeilKleistGao <neilkleist@gmail.com>
2022-09-09 21:46:43 +0800

    Add tests

--- src/test/lua/tup_dec_test.lua
+++ src/test/lua/tup_dec_test.lua
@@ -1,1 +1,8 @@
 local t1 = {1, 2, 3} --[[type: [int, int, int].]]
+local t2 = {"abc", 42} --[[type: [string, int].]]
+function printTuple(x) --[[type: [string, int] -> void.]]
+  return print(x)
+end
+function throwErr(code, err) --[[type: int -> string -> [int, string].]]
+  return {code, err}
+end
